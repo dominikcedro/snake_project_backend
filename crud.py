02de0581e140f608cc01722c98928d53ffd271ce
dominikcedro <dominikcedro00@gmail.com>
2024-10-03 11:58:04 +0000

    changes in crud and ADD hypercorn config realted to different login schema

--- crud.py
+++ crud.py
@@ -74,8 +74,6 @@ def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = pwd_context.hash(user.password)
     db_user = models.User(
         username=user.username,
-        email=user.email,
-        full_name=user.full_name,
         hashed_password=hashed_password,
         disabled=False
     )
@@ -83,3 +81,4 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
---
+++ hypercorn_config.py
@@ -0,0 +1,4 @@
+bind = ["0.0.0.0:443"]
+certfile = "/etc/letsencrypt/live/snakeprojectbackend.duckdns.org/fullchain.pem"
+keyfile = "/etc/letsencrypt/live/snakeprojectbackend.duckdns.org/privkey.pem"
+
